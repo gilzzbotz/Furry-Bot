1dae4bed72f0649cb2ecc77a25df4deb9768663e
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-10-28 06:53:01 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
-│ • Saweria  [https://saweria.co/thesadboy01]
+│ • Tri [PC ATMIN]
+│ • Gopay  [085730794089]
+│ • Dana  [081241056195]
+│ • Saweria  [https://saweria.co/gilzzbotz]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? Wa.me/6285730794089
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
